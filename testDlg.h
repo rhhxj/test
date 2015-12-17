58e2a09861b4d6f8eb32d5a308e56514bfed5920
rhhxj <601605572@qq.com>
2015-12-17 08:59:38 +0800

    The calculator 计算器
    此计算机能够运算简单的加、减、乘、除、退格、清零和去小数点后多余的零等功能

---
+++ testDlg.h
@@ -0,0 +1,56 @@
+
+// testDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CtestDlg 对话框
+class CtestDlg : public CDialogEx
+{
+// 构造
+public:
+	CtestDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_TEST_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CString m_str;
+	double temp;
+	int flag;
+	afx_msg void OnBnClicked1();
+	afx_msg void OnBnClicked2();
+	afx_msg void OnBnClicked3();
+	afx_msg void OnBnClicked4();
+	afx_msg void OnBnClicked5();
+	afx_msg void OnBnClicked6();
+	afx_msg void OnBnClicked7();
+	afx_msg void OnBnClicked8();
+	afx_msg void OnBnClicked9();
+	afx_msg void OnBnClickedAdd();
+	afx_msg void OnBnClickedJian();
+	afx_msg void OnBnClickedCheng();
+	afx_msg void OnBnClickedChu();
+	afx_msg void OnBnClickedcalcuator();
+	afx_msg void OnBnClickedDot();
+	afx_msg void OnBnClickedClear();
+	afx_msg void OnBnClicked0();
+	afx_msg void OnClickedDel();
+	afx_msg void OnClickedButton1();
+	afx_msg void OnClickedPingfang();
+};
